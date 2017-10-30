af772443c02feee24d949ddea71b3d07cc875fd2
wanglittlehandsome <784665813@qq.com>
2017-10-30 13:17:20 +0800

    Create ShirodemoApplication.java

---
+++ src/main/java/com/shiro/example/shirodemo/ShirodemoApplication.java
@@ -0,0 +1,12 @@
+package com.shiro.example.shirodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShirodemoApplication.class, args);
+	}
+}
